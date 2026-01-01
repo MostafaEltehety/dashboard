0ba045c6ad4cbe2ded2b64bc20641722d2671363
mostafa.eltehety@gmail.com <mostafa.eltehety@gmail.com>
2026-01-01 13:34:36 +0200

    n

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,5 @@
 import { Routes } from '@angular/router';
-import { Layout } from './admin/layout/layout';
-import { childRoutes } from './admin/child-routes';
 
 export const routes: Routes = [
-  {
-      path:'',
-      component:Layout,
-      children:[
-        {path:'',
-          redirectTo:'dashboard',pathMatch:'full'
-        },
-        ...childRoutes
-      ]
-    }
+
 ];
