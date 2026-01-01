b1ea62f0e9e40e59c1584353523dc735211586a1
mostafa.eltehety@gmail.com <mostafa.eltehety@gmail.com>
2026-01-01 14:40:42 +0200

    m

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
-import { Component, ViewChild, signal } from '@angular/core';
+ import { Component, ViewChild, signal, Inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { CdkDragEnd, DragDropModule } from '@angular/cdk/drag-drop';
+import { DOCUMENT, NgIf } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { NgIf } from '@angular/common';
- import { MatIconModule } from '@angular/material/icon';
-import { RouterOutlet } from "@angular/router";
+import { MatIconModule } from '@angular/material/icon';
+import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
@@ -18,26 +19,48 @@ import { MatButtonModule } from '@angular/material/button';
     NgIf,
     MatIconModule,
     RouterOutlet,
-    MatButtonModule
+    MatButtonModule,
+    DragDropModule
 ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
 
-
   @ViewChild(MatSidenav) sidenav!: MatSidenav;
 
   isMobile = signal(false);
+  isDark = signal(false);
+  isRtl = signal(false);
 
-  constructor(private breakpoint: BreakpointObserver) {
-    this.breakpoint.observe([
-      Breakpoints.Handset,
-      Breakpoints.Tablet
-    ]).subscribe(result => {
-      this.isMobile.set(result.matches);
-    });
+  constructor(
+    breakpoint: BreakpointObserver,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    breakpoint.observe([Breakpoints.Handset])
+      .subscribe(r => this.isMobile.set(r.matches));
   }
 
+  /* 📱 Swipe */
+  onSwipe(event: CdkDragEnd) {
+    if (!this.isMobile()) return;
+
+    const x = event.distance.x;
+
+    if (x > 80) this.sidenav.open();   // swipe right
+    if (x < -80) this.sidenav.close(); // swipe left
+  }
+
+  /* 🌗 Theme */
+ toggleTheme() {
+  this.isDark.update(v => !v);
+  document.body.classList.toggle('dark-theme', this.isDark());
+}
+
+  /* 🔄 RTL / LTR */
+  toggleDir() {
+    this.isRtl.update(v => !v);
+    this.document.documentElement.dir = this.isRtl() ? 'rtl' : 'ltr';
+  }
 
 }
