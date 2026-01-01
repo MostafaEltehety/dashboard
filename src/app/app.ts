b6f2c76ad0ed77aaf4f205bf6651603cfb622cfa
mostafa.eltehety@gmail.com <mostafa.eltehety@gmail.com>
2026-01-01 14:24:12 +0200

    c

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,89 +1,43 @@
- import {
-  AfterViewInit,
-  Component,
-  OnDestroy,
-  OnInit,
-  ViewChild
-} from '@angular/core';
-
-import { MatSidenav, MatSidenavContainer } from '@angular/material/sidenav';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild, signal } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { map, Subscription } from 'rxjs';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgIf } from '@angular/common';
+ import { MatIconModule } from '@angular/material/icon';
+import { RouterOutlet } from "@angular/router";
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet
-  ],
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    NgIf,
+    MatIconModule,
+    RouterOutlet,
+    MatButtonModule
+],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App implements OnInit, AfterViewInit, OnDestroy {
+export class App {
 
-  @ViewChild(MatSidenavContainer) sidenavContainer!: MatSidenavContainer;
-  @ViewChild('snav') sideNav!: MatSidenav;
 
-  sideNavDefaultOpened = true;
-  showFullMenu = true;
-  isExpanded = true;
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
 
-  closedWidth = 90;
-  openedWidth = 250;
+  isMobile = signal(false);
 
-  isMobile = false;
-  sideNavMode: 'side' | 'over' = 'side';
-  hasBackdrop = false;
-  toolBarHeight = 64;
-
-  private mediaSub!: Subscription;
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
-  ngOnInit(): void {
-    this.mediaSub = this.breakpointObserver
-      .observe([Breakpoints.XSmall, Breakpoints.Small])
-      .pipe(map(result => result.matches))
-      .subscribe(isMobile => {
-        this.isMobile = isMobile;
-
-        if (isMobile) {
-          this.sideNavDefaultOpened = false;
-          this.isExpanded = false;
-          this.sideNavMode = 'over';
-          this.hasBackdrop = true;
-          this.toolBarHeight = 56;
-          this.showFullMenu = true;
-        } else {
-          this.sideNavDefaultOpened = true;
-          this.isExpanded = true;
-          this.sideNavMode = 'side';
-          this.hasBackdrop = false;
-          this.toolBarHeight = 64;
-          this.showFullMenu = true;
-        }
-      });
+  constructor(private breakpoint: BreakpointObserver) {
+    this.breakpoint.observe([
+      Breakpoints.Handset,
+      Breakpoints.Tablet
+    ]).subscribe(result => {
+      this.isMobile.set(result.matches);
+    });
   }
 
-  ngAfterViewInit(): void {
-    this.sidenavContainer.scrollable
-      .elementScrolled()
-      .subscribe(() => {
-        // scroll logic if needed
-      });
-  }
 
-  ngOnDestroy(): void {
-    this.mediaSub?.unsubscribe();
-  }
-
-  onToolbarMenuToggle(): void {
-    if (this.isMobile) {
-      this.sideNav.toggle();
-    } else {
-      this.showFullMenu = !this.isExpanded;
-      this.isExpanded = !this.isExpanded;
-    }
-  }
 }
